2a45fabf3f9fde73b18e4a59d12028d1c6ac1352
christ31 <natanniel31@gmail.com>
2023-05-18 18:19:28 +0700

    v0.7 "Hotfix" - Fix Z-index issue - Redesign Blog Post Card - Teaser for the next project

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
         'color-discord': '#5865f2',
         'color-github': '#171515',
         'color-miku': '#14b8a6',
+        'color-wordpress': '#21759b',
         'color-c31-primary': '#23252e',
         'color-c31-secondary': '#4e5468',
         'color-getwaves-default': '#0066FF',
